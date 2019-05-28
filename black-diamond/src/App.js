308167e67672e2bde55df1433b2802fe6b78dc9f
Todd Hipps <toddhipps@Todds-MacBook-Pro.local>
2019-05-28 14:02:11 -0600

    BDiamond - Complete

--- black-diamond/src/App.js
+++ black-diamond/src/App.js
@@ -12,6 +12,9 @@ class App extends Component {
       cart: [],
       showCart: false
     };
+    this.addToCart = this.addToCart.bind(this)
+    this.removeFromCart = this.removeFromCart.bind(this)
+    this.navigate = this.navigate.bind(this)
   }
   componentDidMount() {
     axios
@@ -36,19 +39,26 @@ class App extends Component {
   }
   navigate(location) {
     if (location === "cart") {
-      this.state.showCart = true;
+      this.setState({
+        showCart: true
+      })
+      
     } else {
-      this.state.showCart = false;
+      this.setState({
+        showCart: false
+      })
+      
     }
   }
   render() {
-    const { products, cart, showCart } = this.state;
+    console.log(this.state.cart)
+    const { products, showCart } = this.state;
     return (
       <div className="App">
         <NavBar navigate={this.navigate} />
         <div className="main-container">
           {showCart ? (
-            <ShoppingCart cart={cart} removeFromCart={this.removeFromCart} />
+            <ShoppingCart cart={this.state.cart} removeFromCart={this.removeFromCart} />
           ) : (
             <StoreFront products={products} addToCart={this.addToCart} />
           )}
